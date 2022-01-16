bfda241a212de4c64c299f67f2e4106ce03d5042
Leon Chan <leonc3490@gmail.com>
2022-01-16 03:14:43 +0000

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.save(new User("Leon", "Chan","leonc3490@gmail.com"));
+		this.userRepository.save(new User("2", "Chan","2@gmail.com"));
+		this.userRepository.save(new User("3", "Chan","3@gmail.com"));
+	}
+}
